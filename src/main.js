1b8b45a846c12de4382c1d8e3b3f91dd98baccb0
Connor Burgess <connorburgesscodes@gmail.com>
2021-02-11 16:34:08 -0800

    refactor code
    Co-authored-by: David Couch <dcouch440@gmail.com>
    Co-authored-by: Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-
-/* eslint-disable */
 import PokeService from './js/poke-service.js';
 import BikeService from './js/bike-service.js';
 import $ from 'jquery';
@@ -13,11 +11,10 @@ async function makeCall(city) {
     response.bikes.map(
       (data, i) => {
         const {title, thumb, description, stolen_location} = data;
-        
         return`
           <div class="new-entry">
             <h3>Someone lost a ${title}</h3>
-            <img class="picture-${i + 1}" src="${isContent(thumb, bikeBasic)}">
+            <img id="picture-${i + 1}" src="${isContent(thumb, bikeBasic)}">
             <div class="location">
               ${isContent(stolen_location)}
             </div>
@@ -28,27 +25,19 @@ async function makeCall(city) {
         `
       }
     ).join('');
-    console.log('bikke ++' + performance.now())
 }
 const random = (x) => Math.floor(Math.random() * x + 1);
 const replaceCard = async  () => {
-  const randomCard = random(7);
-  const randomPokemon = random(1);
+  const randomCard = random(6);
+  const randomPokemon = random(140);
   const response = await PokeService.getPokemon(randomPokemon)
-  // console.log(
-  //   response.sprites.front_shiny,
-  //   document.querySelectorAll(`img`)
-  // );
-  console.log( "Poke" + performance.now())
-  // document.getElementById(`picture-${randomCard}`).src = await response.sprites.front_shiny;
+  document.getElementById(`picture-${randomCard}`).src = response.sprites.front_shiny;
 }
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  
-  makeCall('portland')
-  replaceCard()
-  // $('#click-me').click(function(event) {
-  // const city = $('#city').val(); 
-  // })
+$(document).ready(function() { 
+  $('#click-me').click(function(event) {
+    const city = $('#city').val(); 
+    makeCall(city).then(() => replaceCard());
+  })
 });
